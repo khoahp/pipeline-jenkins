3e48eb84f6ee1122dec07bd80bc0adebc0bf90e0
khoa <khoavd.it@gmail.com>
2021-05-19 17:24:20 +0700

    Update

--- src/main/java/com/khoavu/pipeline/init.groovy
+++ src/main/java/com/khoavu/pipeline/init.groovy
@@ -29,10 +29,10 @@ node {
 def simpleFunc() {
     println("Hello guy")
 
-    String config = libraryResource 'config.json'
+    //String config = libraryResource 'config.json'
 
-    //def props = readJSON file: 'smocktest.json'
-    def props = readJSON text: config
+    def props = readJSON file: 'smocktest.json'
+    //def props = readJSON text: config
     //def props = readJSON text: '{"tools-devportals":" 01-12-2021","mobile-authz":" 01-11-2021"}'
 
     props.each { key, value ->
