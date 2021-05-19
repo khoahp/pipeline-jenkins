c6e9626f1b21f936c51f4c19e5736c3209b0a59c
khoa <khoavd.it@gmail.com>
2021-05-19 18:46:36 +0700

    update

--- init.groovy
+++ init.groovy
@@ -2,10 +2,13 @@
 
 @Library(['pipeline-library-khoavu', 'lib-pipeline'])_
 
+import com.cleverbuilder.GlobalVars
 
 stage('Demo') {
 
     echo 'Hello World'
 
     sayHello 'Dave'
+
+    println GlobalVars.foo
 }
