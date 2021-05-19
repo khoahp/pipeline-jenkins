c877e3e67e60065012b8bfafeffc2bbb8656445b
khoa <khoavd.it@gmail.com>
2021-05-19 22:01:15 +0700

    update

--- init.groovy
+++ init.groovy
@@ -4,6 +4,7 @@
 
 import org.example.Constants
 
+/*
 stage('Demo') {
 
     echo 'Hello World'
@@ -13,4 +14,29 @@ stage('Demo') {
     println('******')
 
     println(Constants.SLACK_MESSAGE)
+}*/
+
+pipeline {
+    agent {
+        label '!windows'
+    }
+
+    environment {
+        DISABLE_AUTH = 'true'
+        DB_ENGINE    = 'sqlite'
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                sayHello 'Dave'
+
+                println(Constants.SLACK_MESSAGE)
+
+                echo "Database engine is ${DB_ENGINE}"
+                echo "DISABLE_AUTH is ${DISABLE_AUTH}"
+                sh 'printenv'
+            }
+        }
+    }
 }
